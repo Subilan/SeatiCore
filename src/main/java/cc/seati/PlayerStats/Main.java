1b26aa097c7709d3d51c2c8b13a306255847d895
Subilan <suyuanliang1995@gmail.com>
2024-07-03 19:50:39 +0800

    feat: add dist specific checks

--- src/main/java/cc/seati/PlayerStats/Main.java
+++ src/main/java/cc/seati/PlayerStats/Main.java
@@ -1,6 +1,8 @@
 package cc.seati.PlayerStats;
 
 import cc.seati.PlayerStats.Database.Database;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.logging.LogManager;
@@ -11,9 +13,15 @@ import java.util.logging.Logger;
 public final class Main {
     public static final String MOD_ID = "playerstats";
     public static final Logger LOGGER = LogManager.getLogManager().getLogger("PlayerStats");
-
     public static void init() {
-        Config.init();
-        Database.init();
+        DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> {
+            LOGGER.info("Initializing PlayerStats...");
+            Config.init();
+            Database.init();
+            LOGGER.info("Initialization end.");
+        });
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> {
+            LOGGER.warning("Invalid dist. This mod can only be enabled on server side.");
+        });
     }
 }
