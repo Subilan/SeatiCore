398408bd7f20dbed3ecbe51ddf2c13ef10c7659b
Subilan <suyuanliang1995@gmail.com>
2024-07-04 15:29:50 +0800

    chore: utils

--- src/main/java/cc/seati/PlayerStats/Utils.java
+++ src/main/java/cc/seati/PlayerStats/Utils.java
@@ -1,12 +1,33 @@
 package cc.seati.PlayerStats;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Objects;
 
 public class Utils {
     public static ServerPlayer getServerPlayer(Player p) {
         return Objects.requireNonNull(p.getServer()).getPlayerList().getPlayer(p.getUUID());
+    }
+
+    public static Timestamp getCurrentTimestamp() {
+        return new Timestamp(new Date().getTime());
+    }
+
+    public static String getPeriodTag() {
+        return Config.t.getString("period-tag", "default");
+    }
+
+    public static void sendAll(MinecraftServer server, Component component) {
+        Objects.requireNonNull(server)
+                .getPlayerList()
+                .getPlayers()
+                .forEach(
+                        p -> p.sendSystemMessage(component)
+                );
     }
 }
