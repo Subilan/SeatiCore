effc5b70e894e742d7af207e666ccf9ffa336629
Subilan <suyuanliang1995@gmail.com>
2024-07-13 21:39:40 +0800

    fix: shutdown server properly

--- src/main/java/cc/seati/PlayerStats/Events/ServerEvents.java
+++ src/main/java/cc/seati/PlayerStats/Events/ServerEvents.java
@@ -2,10 +2,12 @@ package cc.seati.PlayerStats.Events;
 
 import cc.seati.PlayerStats.Main;
 import cc.seati.PlayerStats.Tracker.PlayersOnlineTracker;
+import cc.seati.PlayerStats.Utils.CommonUtil;
 import cc.seati.PlayerStats.Utils.ConfigUtil;
 import cc.seati.PlayerStats.WebSocket.WebSocketServer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -23,5 +25,17 @@ public class ServerEvents {
             Main.wsThread = new Thread(() -> Main.wsServer.run());
             Main.wsThread.start();
         }
+    }
+
+    @SubscribeEvent
+    public static void onServerStopping(ServerStoppingEvent e) {
+        CommonUtil.tryRun(() -> {
+            Main.wsServer.stop();
+            Main.playersOnlineTracker.shutdown();
+            return null;
+        }, ex -> {
+            Main.LOGGER.error("Some external threads are unable to stop. Please try using Ctrl+C to resolve.");
+            return null;
+        });
     }
 }
