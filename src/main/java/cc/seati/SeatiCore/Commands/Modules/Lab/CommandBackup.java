8b5c56d4f43dcb98fc382f50e8e5ba307d4f32e1
Subilan <suyuanliang1995@gmail.com>
2024-08-06 21:31:15 +0800

    feat: manual archive & backup

---
+++ src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/CommandBackup.java
@@ -0,0 +1,18 @@
+package cc.seati.SeatiCore.Commands.Modules.Lab;
+
+import cc.seati.SeatiCore.Commands.Abstract.Command;
+import cc.seati.SeatiCore.Utils.CommonUtil;
+import cc.seati.SeatiCore.Utils.ConfigUtil;
+import cc.seati.SeatiCore.Utils.OSSUtil;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+
+public class CommandBackup extends Command {
+    @Override
+    public int handle(CommandContext<CommandSourceStack> ctx) {
+        CommonUtil.sendMessage(ctx, "&e开始备份，服务器线程在此过程将被阻塞至多 &c" + ConfigUtil.getOssUploadTimeout() + "s");
+        OSSUtil.doBackup();
+        CommonUtil.sendMessage(ctx, "&e执行完毕");
+        return 1;
+    }
+}
--- src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/ModuleLab.java
+++ src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/ModuleLab.java
@@ -7,12 +7,14 @@ import net.minecraft.commands.CommandSourceStack;
 import java.util.List;
 
 public class ModuleLab extends Module {
-    public static final List<String> ACTIONS = List.of("verify");
+    public static final List<String> ACTIONS = List.of("verify", "archive", "backup");
 
     @Override
     public int handle(CommandContext<CommandSourceStack> ctx, String module, String action, String[] argArray) {
         return switch (action) {
             case "verify" -> new CommandVerify().handle(ctx);
+            case "archive" -> new CommandArchive().handle(ctx);
+            case "backup" -> new CommandBackup().handle(ctx);
             default -> throw new IllegalStateException("Unexpected value: " + action);
         };
     }
