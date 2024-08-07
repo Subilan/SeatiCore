9610187455d1a0adcd189b097a15da6c573d6b43
Subilan <suyuanliang1995@gmail.com>
2024-08-07 16:32:22 +0800

    chore: add manual note

--- src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/CommandArchive.java
+++ src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/CommandArchive.java
@@ -10,7 +10,7 @@ import net.minecraft.commands.CommandSourceStack;
 public class CommandArchive extends Command {
     @Override
     public int handle(CommandContext<CommandSourceStack> ctx) {
-        CommonUtil.sendMessage(ctx, "&e开始归档，服务器线程在此过程将被阻塞至多 &c" + ConfigUtil.getOssUploadTimeout() + "s");
+        CommonUtil.sendMessage(ctx, "&e开始手动归档，服务器线程在此过程将被阻塞至多 &c" + ConfigUtil.getOssUploadTimeout() + "s");
         OSSUtil.doArchive();
         CommonUtil.sendMessage(ctx, "&e执行完毕");
         return 1;
--- src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/CommandBackup.java
+++ src/main/java/cc/seati/SeatiCore/Commands/Modules/Lab/CommandBackup.java
@@ -10,7 +10,7 @@ import net.minecraft.commands.CommandSourceStack;
 public class CommandBackup extends Command {
     @Override
     public int handle(CommandContext<CommandSourceStack> ctx) {
-        CommonUtil.sendMessage(ctx, "&e开始备份，服务器线程在此过程将被阻塞至多 &c" + ConfigUtil.getOssUploadTimeout() + "s");
+        CommonUtil.sendMessage(ctx, "&e开始手动备份，服务器线程在此过程将被阻塞至多 &c" + ConfigUtil.getOssUploadTimeout() + "s");
         OSSUtil.doBackup();
         CommonUtil.sendMessage(ctx, "&e执行完毕");
         return 1;
